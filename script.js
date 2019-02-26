18868dfd82e57e04974e6dbd7b5512e4c1afbcfd
Mikel McCallum <mikelmccallum@Mikels-MacBook-Air.local>
2019-02-26 13:23:49 -0500

    initial commit project finished

---
+++ script.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+
+
+
+    $("#4πr2").on("click", function() {
+        alert("NICE!!!!!")
+    });
+
+    $("#18").on("click", function() {
+        alert("GREAT")
+
+    })
+    $("#300").on("click", function () {
+        alert("NICE");
+
+
+    });
+    $("400").on("click", function () {
+        alert("YOU DID IT")
+
+    })
+    $("#500").on("click", function () {
+        alert("GREAT!!!!")
+
+    })
+
+
+    $("#100").on("click", function() {
+        alert("YOU GOT IT");
+    });
+
+
+    $("#200").on("click", function () {
+        alert("GREAT JOB!!!!!!")
+    });
+
+
+    $("#atom").on("click", function() {
+        alert("GOOD JOB");
+
+    });
+
+    $("#400").on("click", function () {
+        alert("GREAT!!!!!!")
+
+    })
+    $("#500").on("click", function () {
+        alert("AWESOME!!!!!")
+
+    })
+
+
+    $("#100").on("click", function () {
+        alert("FANTASTIC!!!!");
+
+    });
+
+
+    $("#GeorgeWashington").on("click", function () {
+        alert("YOU GOT IT!!!!!");
+    });
+
+
+    $("#300").on("click", function () {
+        alert("YOUR ON A ROLL!!!!!")
+
+    })
+    $("#400").on("click", function () {
+        alert("GREAT")
+
+    })
+    $("#JackTheRipper").on("click", function () {
+        alert("NICE")
+
+    })
+ })
